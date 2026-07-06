2dfa7239d1a7e0744bb72b1ed181822ad4a4e9e1
bon <bon3409@gmail.com>
2026-07-06 23:36:39 +0800

    Create Grafana dashboard, alarm by terraform

--- terraform/grafana/outputs.tf
+++ terraform/grafana/outputs.tf
@@ -1,7 +1,3 @@
 output "rule_folder" {
   value = { for key, folder in grafana_folder.rule_folder : key => folder }
 }
-
-# output "alert_uids" {
-#   value = [for k, v in grafana_rule_group.dcgm_alerts : v.rule[0].uid]
-# }
